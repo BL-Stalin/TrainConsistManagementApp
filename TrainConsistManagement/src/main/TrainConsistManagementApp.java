432feffaaae1672fa2112ba8cca07e79e2379da3
Stalin <253462185+BL-Stalin@users.noreply.github.com>
2026-06-27 10:00:00 +0530

    [Stalin] Added : UC17 Sort Bogie Names Using Arrays.sort()

--- TrainConsistManagement/src/main/TrainConsistManagementApp.java
+++ TrainConsistManagement/src/main/TrainConsistManagementApp.java
@@ -1,30 +1,17 @@
 package main;
 
+import java.util.Arrays;
+
 public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
-        System.out.println(
-                "=== Train Consist Management App ===");
-        int[] capacities = {72, 24, 56, 90, 48};
-        System.out.println("\nPassenger Bogie Capacities Before Sorting:");
-        displayArray(capacities);
-        for (int i = 0; i < capacities.length - 1; i++) {
-            for (int j = 0; j < capacities.length - 1 - i; j++) {
-                if (capacities[j] > capacities[j + 1]) {
-                    int temp = capacities[j];
-                    capacities[j] = capacities[j + 1];
-                    capacities[j + 1] = temp;
-                }
-            }
-        }
-        System.out.println("\nPassenger Bogie Capacities After Sorting:");
-        displayArray(capacities);
-    }
-
-    private static void displayArray(int[] capacities) {
-        for (int capacity : capacities) {
-            System.out.print(capacity + " ");
-        }
-        System.out.println();
+        System.out.println("=== Train Consist Management App ===");
+        String[] bogieNames = {"Sleeper", "First Class", "Cargo", "AC Chair", "Guard"};
+        System.out.println("\nBefore Sorting:");
+        System.out.println(Arrays.toString(bogieNames));
+        // Sort Alphabetically
+        Arrays.sort(bogieNames);
+        System.out.println("\nAfter Sorting:");
+        System.out.println(Arrays.toString(bogieNames));
     }
 }
